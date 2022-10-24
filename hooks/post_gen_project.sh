9676484332ef591c31875dd9d0117c763f631b5a
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2022-10-24 15:10:12 +0200

    Ensure that git commit doesn't fail when no username or email has been configured in git

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,4 +2,10 @@
 
 git init
 git add .
+username="inmanta-project-template"
+email=""
+export GIT_COMMITTER_NAME="${username}"
+export GIT_COMMITTER_EMAIL="${email}"
+export GIT_AUTHOR_NAME="${username}"
+export GIT_AUTHOR_EMAIL="${email}"
 git commit -m "initial commit: cookiecutter template"
